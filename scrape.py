e5891400a6adfea75f3f58cc9aa82e2ce0298a7f
Colin Taylor <colin2328@gmail.com>
2015-11-28 23:56:02 -0800

    added command line running ability- added results.txt

--- scrape.py
+++ scrape.py
@@ -3,67 +3,74 @@ import cookielib
 import time
 import sys
 import os
+import argparse
 
 from parse import print_results_from_page, get_number_of_pages
 from user_agent import get_agent
 
-# Browser
-br = mechanize.Browser()
 
-# Cookie Jar
-cj = cookielib.LWPCookieJar()
-br.set_cookiejar(cj)
+def save_results(city_name='San Francisco'):
+    # Browser
+    br = mechanize.Browser()
 
-# Browser options
-br.set_handle_equiv(True)
-# br.set_handle_gzip(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
+    # Cookie Jar
+    cj = cookielib.LWPCookieJar()
+    br.set_cookiejar(cj)
 
-# Follows refresh 0 but not hangs on refresh > 0
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+    # Browser options
+    br.set_handle_equiv(True)
+    # br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
 
-# Want debugging messages?
-# br.set_debug_http(True)
-# br.set_debug_redirects(True)
-# br.set_debug_responses(True)
+    # Follows refresh 0 but not hangs on refresh > 0
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-br.addheaders = [('User-agent', get_agent())]
-r = br.open('http://provider.bcbs.com/Search/')
+    # Want debugging messages?
+    # br.set_debug_http(True)
+    # br.set_debug_redirects(True)
+    # br.set_debug_responses(True)
 
-try:
-    br.select_form(nr=0)
-except mechanize.FormNotFoundError:
-    print 'try refreshing browser- it thinks we\'re a bot'
-    with open("error.html", "w") as f:
-            f.write(br.response().read())
-    sys.exit()
+    br.addheaders = [('User-agent', get_agent())]
+    r = br.open('http://provider.bcbs.com/Search/')
 
-
-br.form['Keyword'] = 'lpc'
-br.form['Address'] = 'San Francisco'
-br.form['Proximity'] = ['50']
-br.form['Product'] = ['PPO']
-
-
-br.submit()
-
-html = br.response().read()
-os.remove('results.csv')
-
-print_results_from_page(html)
-num_pages = get_number_of_pages(html)
-
-for page_num in range(2, num_pages + 1):
-    time.sleep(0.5)
-    url = 'http://provider.bcbs.com/ReturnToResults?PageRequested={}'.format(page_num)
-
-    br.open(url)
-    html = br.response().read()
     try:
-        print_results_from_page(html)
-    except Exception:
+        br.select_form(nr=0)
+    except mechanize.FormNotFoundError:
+        print 'try refreshing browser- it thinks we\'re a bot'
         with open("error.html", "w") as f:
-            f.write(html)
-            break
+                f.write(br.response().read())
+        sys.exit()
+
+    br.form['Keyword'] = 'lpc'
+    br.form['Address'] = city_name
+    br.form['Proximity'] = ['50']
+    br.form['Product'] = ['PPO']
+
+    br.submit()
+
+    html = br.response().read()
+    os.remove('results.csv')
+
+    print_results_from_page(html)
+    num_pages = get_number_of_pages(html)
+
+    for page_num in range(2, num_pages + 1):
+        time.sleep(0.5)
+        url = 'http://provider.bcbs.com/ReturnToResults?PageRequested={}'.format(page_num)
+
+        br.open(url)
+        html = br.response().read()
+        try:
+            print_results_from_page(html)
+        except Exception:
+            with open("error.html", "w") as f:
+                f.write(html)
+                break
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get results for a city.')
+    parser.add_argument('--city', nargs='+', help='City to search for')
+    args = parser.parse_args()
+    save_results(city_name=' '.join(args.city))
