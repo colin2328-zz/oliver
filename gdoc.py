ae424241ea1cb41d166a1917a38ded50f5ded1b0
Colin Taylor <colin2328@gmail.com>
2015-11-30 15:00:55 -0800

    working gdoc

--- gdoc.py
+++ gdoc.py
@@ -16,10 +16,23 @@ def populate_gdoc():
     worksheet = sht.get_worksheet(0)
 
     with open('results.csv', 'r') as f:
-        data = []
         reader = csv.reader(f, delimiter=',')
+        row_count = sum(1 for row in reader)
+
+    with open('results.csv', 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        first_new_row = worksheet.row_count + 1
+        last_new_row = first_new_row + row_count
+        worksheet.add_rows(row_count)
+        cell_list = worksheet.range('A{}:I{}'.format(first_new_row, last_new_row - 1))
+
+        i = 0
         for row in reader:
             first, last, creds, address, city, state, zipcode, phone = row
             row_data = [first, last, creds, '', phone, address, city, state, zipcode]
-            data.append(row_data)
-            # worksheet.append_row()
+            for value in row_data:
+                cell_list[i].value = value
+                i += 1
+
+    assert (i == len(cell_list))
+    worksheet.update_cells(cell_list)
